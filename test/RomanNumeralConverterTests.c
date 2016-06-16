0b161faf1bbccda08cb265b69abfbd6ec8753dd6
Brett Lilley <blilley@pillartechnology.com>
2016-06-16 18:53:39 -0400

    Failing test for three Is

--- test/RomanNumeralConverterTests.c
+++ test/RomanNumeralConverterTests.c
@@ -7,6 +7,8 @@ START_TEST(test_standardRomanNumerals_ReturnsExpected)
    {
        ck_assert_int_eq(toArabic("I"), 1);
        ck_assert_int_eq(toArabic("II"), 2);
+       ck_assert_int_eq(toArabic("III"), 3);
+
 
    }
 END_TEST
