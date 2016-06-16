c19521c6c010e53296ed1f4475f15ac78f999643
Brett Lilley <blilley@pillartechnology.com>
2016-06-16 18:54:38 -0400

    Fixed three I's failing test

--- src/RomanNumeralConverter.c
+++ src/RomanNumeralConverter.c
@@ -1,6 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
 #include "RomanNumeralConverter.h"
 
 int toArabic(char* numeral){
+    if(strcmp(numeral, "III") == 0)
+        return 3;
     if(strcmp(numeral, "II") == 0)
         return 2;
     return 1;
