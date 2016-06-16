cd12b1bdf0ebf6d47427af0c2bb165bd85e610ee
Brett Lilley <blilley@pillartechnology.com>
2016-06-16 18:29:53 -0400

    Test Runner walking skeleton

--- test/TestRunner.c
+++ test/TestRunner.c
@@ -2,6 +2,11 @@
 #include <check.h>
 
 int main(){
-    printf("Test\n");
-    return 0;
+    int numberOfFailedTests;
+    SRunner *suiteRunner = srunner_create(NULL);
+    srunner_run_all(suiteRunner, CK_NORMAL);
+    numberOfFailedTests = srunner_ntests_failed(suiteRunner);
+    srunner_free(suiteRunner);
+
+    return (numberOfFailedTests == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
