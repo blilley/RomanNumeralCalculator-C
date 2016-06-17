6ec799851cf1d7faa09baf9e86b42a68300fabcb
Brett Lilley <blilley@pillartechnology.com>
2016-06-16 21:14:46 -0400

    Refactoring toArabic

--- src/RomanNumeralConverter.c
+++ src/RomanNumeralConverter.c
@@ -1,16 +1,24 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "RomanNumeralConverter.h"
+
+int const arabicValues[3] = {1, 5, 10};
+char const *romanNumerals[3] = {"I", "V", "X"};
+
+int getArabicValue(char* roman){
+    for(int i = 0; i < 3; i++){
+        if(strcmp(romanNumerals[i], roman) == 0)
+            return arabicValues[i];
+    }
+    return -1;
+}
 
 int toArabic(char* numeral){
     int value = 0;
     for(int i = 0; i < strlen(numeral); i++){
-        if(numeral[i] == 'V')
-            value += 5;
-        else if(numeral[i] == 'X')
-            value += 10;
-        else
-            value++;
+        char characterString[2] = {numeral[i], '\0'};
+        value += getArabicValue(characterString);
     }
     return value;
 }
