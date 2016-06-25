6ee285e4ceb4c5d4b078bf4f1a7ac01d908b956c
Brett Lilley <blilley@pillartechnology.com>
2016-06-25 08:18:59 -0400

    Refactored Add and Subtract to take const strings

--- src/RomanNumeralCalculator.c
+++ src/RomanNumeralCalculator.c
@@ -1,9 +1,9 @@
 #include "RomanNumeralConverter.h"
 
-char* addRomanNumerals(char* value1, char* value2){
+char* addRomanNumerals(const char* value1, const char* value2){
     return toRoman(toArabic(value1) + toArabic(value2));
 }
 
-char* subtractRomanNumerals(char* value1, char* value2){
+char* subtractRomanNumerals(const char* value1, const char* value2){
     return toRoman(toArabic(value1) - toArabic(value2));
 }
--- src/RomanNumeralCalculator.h
+++ src/RomanNumeralCalculator.h
@@ -1,6 +1,6 @@
 #ifndef ROMAN_NUMERAL_CALCULATOR_H
 #define ROMAN_NUMERAL_CALCULATOR_H
-char* addRomanNumerals(char* value1, char* value2);
-char* subtractRomanNumerals(char* value1, char* value2);
+char* addRomanNumerals(const char* value1, const char* value2);
+char* subtractRomanNumerals(const char* value1, const char* value2);
 
 #endif
